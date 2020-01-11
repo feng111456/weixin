d37ebd7a6395ede548ee6cb358fa01c158cda528
panfeng <983660431@qq.com>
2020-01-11 15:57:26 +0800

    weixin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    攀峰
+                    攀峰123
                 </div>
 
                 <div class="links">
